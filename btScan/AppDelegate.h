6c671785b1dacf7149e4e340cc1e7cf62cba3cc4
baron <syhuang75@gmail.com>
2016-07-07 13:38:18 +0800

    Init project.

---
+++ btScan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  btScan
+//
+//  Created by baron on 2016/7/7.
+//  Copyright © 2016年 baron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ btScan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  btScan
+//
+//  Created by baron on 2016/7/7.
+//  Copyright © 2016年 baron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
